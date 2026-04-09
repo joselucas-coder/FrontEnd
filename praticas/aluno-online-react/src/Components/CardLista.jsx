b42a97eaa2c3ffbd585c855e561f828d8c1d5cd3
José Lucas Ferreira de Sousa <sousajoselucas10@gmail.com>
2026-04-09 08:26:55 -0300

    feat: conclui pratica04. Fecha #7

--- praticas/aluno-online-react/src/Components/CardLista.jsx
+++ praticas/aluno-online-react/src/Components/CardLista.jsx
@@ -6,7 +6,7 @@ export default function CardLista({ titulo, itens }) {
         {titulo}
       </div>
       <ul className="p-4 flex flex-col gap-2 text-[0.95rem] text-gray-800">
-        {itens.map((item, index) => (
+        {itens.map((item, index) => ( 
           <li key={index}>{item}</li>
         ))}
       </ul>
